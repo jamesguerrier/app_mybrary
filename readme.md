d9a6130127f175b785d88c21fad3a159cee5cdf7
James Guerrier <jamesguerrier72@gmail.com>
2023-12-04 19:07:17 -0500

    intial setup

---
+++ readme.md
@@ -0,0 +1,7 @@
+##this a full stack app made using
+
+1-nodejs
+2-expressjs
+3-bcrypt
+
+inspiring this youtube play list : https://www.youtube.com/watch?v=qj2oDkvc4dQ&list=PLZlA0Gpn_vH8jbFkBjOuFjhxANC63OmXM&index=5;
---
+++ server.js
@@ -0,0 +1,28 @@
+
+const express = require('express');
+const app = express();
+const expressLayouts = require('express-ejs-layouts');
+const indexRouter = require('./routes/index')
+require('dotenv').config();
+
+
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+app.set('layout', './layouts/layout');
+app.use(expressLayouts);
+app.use(express.static('public'));
+app.use('/', indexRouter)
+
+port = process.env.PORT || 5000;
+
+const mongoose = require('mongoose')
+
+mongoose.connect(process.env.DB_URI);
+const db = mongoose.connection
+db.on('error', error => console.error(error))
+db.once('open', () => console.log('connected to mongodb'))
+
+
+app.listen(port, () => {
+    console.log(`the app is running in port ${port}`)
+})
