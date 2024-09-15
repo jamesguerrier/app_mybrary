f897a99b5f66a061444ccdfd1fe38fe6a7c4ca59
James Guerrier <jamesguerrier72@gmail.com>
2024-09-14 22:10:53 -0500

    add books route to up load books

--- readme.md
+++ readme.md
@@ -5,3 +5,7 @@
 3-bcrypt
 
 inspiring this youtube play list : https://www.youtube.com/watch?v=qj2oDkvc4dQ&list=PLZlA0Gpn_vH8jbFkBjOuFjhxANC63OmXM&index=5;
+
+the deployment in render
+
+https://app-mybrary.onrender.com/
--- server.js
+++ server.js
@@ -4,7 +4,9 @@ const app = express();
 const expressLayouts = require('express-ejs-layouts');
 const indexRouter = require('./routes/index')
 const authorRouter = require('./routes/authors')
+const bookRouter = require('./routes/books')
 require('dotenv').config();
+const bodyParser = require('body-parser');
 
 
 app.set('view engine', 'ejs');
@@ -12,9 +14,10 @@ app.set('views', __dirname + '/views');
 app.set('layout', './layouts/layout');
 app.use(expressLayouts);
 app.use(express.static('public'));
-app.use(express.urlencoded({ extended: true }))
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }))
 app.use('/', indexRouter)
 app.use('/authors', authorRouter)
+app.use('/books', bookRouter)
 
 port = process.env.PORT || 5000;
 
